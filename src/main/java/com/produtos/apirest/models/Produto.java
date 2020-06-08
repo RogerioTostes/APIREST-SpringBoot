6822bc90f3eaa4bf48002b84f74ea62623f6ee06
RogerioTostes <40499106+RogerioTostes@users.noreply.github.com>
2020-06-07 22:17:58 -0300

    add finish project

--- src/main/java/com/produtos/apirest/models/Produto.java
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
+
 @Entity
 @Table(name="TB_PRODUTO")
 public class Produto implements Serializable{
@@ -18,44 +20,42 @@ public class Produto implements Serializable{
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
-		
+	
+
 	private String nome;
+	
 
 	private BigDecimal quantidade;
 	
-	private BigDecimal valor;
 
+	private BigDecimal valor;
+	
+	
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public BigDecimal getQuantidade() {
 		return quantidade;
 	}
-
 	public void setQuantidade(BigDecimal quantidade) {
 		this.quantidade = quantidade;
 	}
-
 	public BigDecimal getValor() {
 		return valor;
 	}
-
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
 	
 	
+
 }
